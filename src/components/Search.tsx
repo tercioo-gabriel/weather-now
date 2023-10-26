895a18ce6c4d8ef6c3af7e8014124e6dbf980748
Tercio Gabriel <terciogabrieldejesus@gmail.com>
2023-10-26 11:30:22 -0300

    função Loading implementada

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,21 +1,32 @@
 import React, { useState } from 'react';
 import { RiSearch2Line } from 'react-icons/ri';
+import { AiOutlineLoading } from 'react-icons/ai'
 
 interface SearchProps {
-  onSearch: (city: string) => void;
+  onSearch: (city: string) => Promise<void>;
 }
 
 const Search: React.FC<SearchProps> = ({ onSearch }) => {
   const [searchedCity, setSearchedCity] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = () => {
     if (searchedCity) {
-      onSearch(searchedCity);
+      setIsLoading(true);
+      
+      onSearch(searchedCity)
+        .then(() => {
+          setIsLoading(false);
+        })
+        .catch(error => {
+          console.error("Ocorreu um erro:", error);
+          setIsLoading(false);
+        });
     }
   }
 
   const handleKeyDown = (e: any) => {
-    if (e.code === "Enter") {
+    if (e.key === "Enter") {
     handleSearch();
     }
   };
@@ -32,7 +43,13 @@ const Search: React.FC<SearchProps> = ({ onSearch }) => {
       />
       <button 
         onClick={handleSearch}
-        className="ml-2 text-2xl bg-zinc-200 text-zinc-500 px-2.5 h-10 rounded-md transition-all duration-150 sm:hover:pr-5 sm:hover:text-zinc-700"><RiSearch2Line /></button>
+        className="ml-2 text-2xl bg-zinc-200 text-zinc-500 px-2.5 h-10 rounded-md transition-all duration-150 sm:hover:pr-5 sm:hover:text-zinc-700">
+          {isLoading ? (
+          <AiOutlineLoading className="animate-spin" />
+        ) : (
+          <RiSearch2Line />
+        )}
+        </button>
     </div>
   );
 }
